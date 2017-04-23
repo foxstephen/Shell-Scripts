bc0d8e46a5ddabe6923ff4c0d3c436ceda5f7330
Stephen Fox <stephenfox995@hotmail.com>
2017-04-23 14:06:12 +0100

    Added motd

--- weather.py
+++ weather.py
@@ -1,7 +1,9 @@
+import sys
 try:
 	import requests
 except:
 	exit()
+
 
 def parse_weather_short(weather):
 	lines = weather.split('\n')
@@ -10,6 +12,13 @@ def parse_weather_short(weather):
 		parsed += (lines[_] + '\n')
 	return parsed
 
-r = requests.get('http://wttr.in/Dublin')
-print(r.text)
-print(parse_weather_short(r.text))
+if __name__ == "__main__":
+	if len(sys.argv) < 2:
+		print('Not enough arguments for weather.py')
+		exit()
+
+	url = 'http://wttr.in/%s' % (sys.argv[1])
+	r = requests.get(url)
+	if r.status_code != 200:  # something went wrong, just exit
+		exit()
+	print(parse_weather_short(r.text.encode('utf-8')))
